317592f815dd424a7e8bde248361b3f273d4fdde
Roman Heinrich <roman.heinrich@gmail.com>
2021-04-03 20:50:33 +0200

    Try different configs

--- config/releases.exs
+++ config/releases.exs
@@ -13,12 +13,16 @@ secret_key_base =
 config :demo, DemoWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
+    transport_options: [socket_opts: [:inet6]],
+    # port: {:system, "PORT"}],
+    url: [scheme: "https", host: "vite-phoenix.herokuapp.com"],
+    port: 443
   ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: secret_key_base,
-  url: [host: "example.com", port: 80],
+  url: [host: "vite-phoenix.herokuapp.com", port: 80],
   # this is needed for our LiveSocket / Websockets
-  check_origin: ["https://example.com", "//localhost:4000"],
+  check_origin: ["https://example.com", "//localhost:4000", "//vite-phoenix.herokuapp.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # start HTTP server
